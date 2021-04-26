fbfdcf2eacecb2cc17fff848156759bf11ab0a6a
Eser Alexander (s) <alexander.eser@students.fhnw.ch>
2021-04-26 15:07:13 +0200

    Implemented touch interaction - TouchPhase.Moved to rotate the picture on the smartphone.

--- 360 Viewer/Assets/rotate.cs
+++ 360 Viewer/Assets/rotate.cs
@@ -26,12 +26,28 @@ public class rotate : MonoBehaviour
 		float scroll = Input.GetAxis("Mouse ScrollWheel");
 		transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
 
-		if (Input.GetMouseButton(0))
+		foreach (Touch touch in Input.touches)
 		{
-			rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime * 30;
-			rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime * 30;
-			rotationY = Mathf.Clamp(rotationY, minY, maxY);
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			//Input.GetMouseButton(0) || 
+			if (touch.phase == TouchPhase.Moved)
+			{
+				rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+				rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+				rotationY = Mathf.Clamp(rotationY, minY, maxY);
+				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			}
 		}
+		
+		
+		if (Input.touchCount >= 2)
+		{
+			Vector2 touch0, touch1;
+			float distance;
+			touch0 = Input.GetTouch(0).position;
+			touch1 = Input.GetTouch(1).position;
+			distance = Vector2.Distance(touch0, touch1);
+		}       
+		
+		
 	}
 }
