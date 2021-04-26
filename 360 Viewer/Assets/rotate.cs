45c4703f0de65573ce2ccac64022009275d8e016
galliums <janosch.hemmi@hotmail.com>
2021-04-26 12:37:59 +0200

    First commit 360 Viewer

---
+++ 360 Viewer/Assets/rotate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+
+	private float speed = 2.0f;
+	private float zoomSpeed = 2.0f;
+
+	public float minX = -360.0f;
+	public float maxX = 360.0f;
+
+	public float minY = -45.0f;
+	public float maxY = 45.0f;
+
+	public float sensX = 100.0f;
+	public float sensY = 100.0f;
+
+	float rotationY = 0.0f;
+	float rotationX = 0.0f;
+
+	void Update()
+	{
+
+		float scroll = Input.GetAxis("Mouse ScrollWheel");
+		transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+
+		if (Input.GetMouseButton(0))
+		{
+			rotationX += Input.GetAxis("Mouse X") * sensX * Time.deltaTime;
+			rotationY += Input.GetAxis("Mouse Y") * sensY * Time.deltaTime;
+			rotationY = Mathf.Clamp(rotationY, minY, maxY);
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+	}
+}
