c134f8c4410adf759eded0f4af4754a290b5be99
galliums <janosch.hemmi@hotmail.com>
2021-05-31 14:57:10 +0200

    Add all planes

---
+++ 360 Viewer/Assets/ButtonPress.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonPress : MonoBehaviour
+{
+    // Start is called before the first frame update    
+    void Start() { }
+
+    // Update is called once per frame    
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Ended)
+            {
+
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    //Select stage    
+                    if (hit.transform.tag == "Button")
+                    {
+                        SceneManager.LoadScene("Reservation");
+                    }
+                }
+            }
+        }
+    }
+}
